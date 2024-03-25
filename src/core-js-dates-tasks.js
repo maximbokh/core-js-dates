eb47bff3b7ccde86ff3e6790921750e13930b262
maximbokh <isupermaximus@gmail.com>
2024-03-25 16:12:44 +0300

    feat: add solution part 2

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -54,8 +54,11 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const newDate = new Date(date);
+  const options = { weekday: 'long' };
+
+  return new Intl.DateTimeFormat('en-US', options).format(newDate);
 }
 
 /**
@@ -69,8 +72,19 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const newDate = date;
+  const fridayNum = 5;
+  const day = date.getDate();
+  const weekday = date.getDay();
+  const dif = fridayNum - weekday;
+
+  if (dif > 0) {
+    newDate.setDate(day + dif);
+  } else {
+    newDate.setDate(day + (7 + dif));
+  }
+  return newDate;
 }
 
 /**
@@ -84,8 +98,10 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const days = new Date(year, month, 0).getDate();
+
+  return days;
 }
 
 /**
@@ -99,8 +115,13 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const dateSt = new Date(dateStart);
+  const dateEn = new Date(dateEnd);
+  const diff =
+    (dateEn.getTime() - dateSt.getTime()) / (1000 * 60 * 60 * 24) + 1;
+
+  return diff;
 }
 
 /**
