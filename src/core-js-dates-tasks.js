3aa237a5033c384411c87afa102dce151dbdba31
maximbokh <isupermaximus@gmail.com>
2024-03-25 19:48:22 +0300

    feat: add solution part 4

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -141,8 +141,18 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  let result;
+  const curDate = new Date(date).getTime();
+  const startDate = new Date(period.start).getTime();
+  const endDate = new Date(period.end).getTime();
+  if (curDate >= startDate && curDate <= endDate) {
+    result = true;
+  } else {
+    result = false;
+  }
+
+  return result;
 }
 
 /**
